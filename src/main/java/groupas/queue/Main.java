194e4eba4cf0ce44a1053acbfa48fb9b7673542b
Giwrgos Roupas <degen1997@gmail.com>
2024-01-21 07:23:31 +0200

    add test case

--- src/main/java/groupas/queue/Main.java
+++ src/main/java/groupas/queue/Main.java
@@ -3,27 +3,26 @@ package groupas.queue;
 
 public class Main {
     public static void main(String[] args) {
-//        var queue = new DoubleEndedQueue<Integer>();
-//
-//        queue.pushFirst(1);
-//        queue.pushFirst(2);
-//        queue.pushFirst(3);
-//        queue.pushFirst(4);
-//        queue.pushFirst(5);
-//        queue.pushFirst(6);
-//        queue.popFirst();
-//        queue.pushLast(-2);
-//
-//        System.out.println(queue);
-//        queue.popLast();
-//        System.out.println(queue);
+        var queue = new DoubleEndedQueue<Integer>();
 
-        var stringQ = new DoubleEndedQueue<String>();
+        queue.pushFirst(1);
+        queue.pushFirst(2);
+        queue.pushFirst(3);
+        queue.pushFirst(4);
+        queue.pushFirst(5);
+        queue.pushFirst(6);
+        queue.pushFirst(7);
+        queue.pushFirst(8);
+        System.out.println(queue);
+        queue.pushFirst(9);
+        System.out.println(queue);
 
-        stringQ.pushFirst("first");
-        stringQ.pushFirst("second");
-        stringQ.pushLast("third");
-        stringQ.pushLast("fourth");
-        System.out.println(stringQ);
+//        var stringQ = new DoubleEndedQueue<String>();
+//        stringQ.pushFirst("first");
+//        stringQ.pushFirst("second");
+//        stringQ.pushLast("third");
+//        stringQ.pushLast("fourth");
+//        System.out.println(stringQ);
+
     }
 }
